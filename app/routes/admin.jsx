2ec33bd1fe620ee37253d05ad3f1f707ddfd851c
sonnguyenhong <sonnguyenhong291@gmail.com>
2023-08-31 08:39:01 +0700

    hongson-graphql: add logout

--- app/routes/admin.jsx
+++ app/routes/admin.jsx
@@ -1,39 +1,21 @@
-import { json, redirect } from "@remix-run/node";
-import { Outlet, useLoaderData, useNavigate, useSubmit } from "@remix-run/react";
-import { SearchMinor, MetafieldsMinor, DiscountsFilledMinor, MagicMinor, StoreDetailsMinor } from '@shopify/polaris-icons';
-import { ActionList, Button, IndexTable, LegacyCard, Page, Popover, Text, useIndexResourceState } from "@shopify/polaris";
+import { Outlet, useNavigate } from "@remix-run/react";
 import CustomPolarisAppProvider from "~/components/CustomPolarisAppProvider";
 import indexStyles from "./_index/style.css";
-import { useCallback, useState } from "react";
-import StoreServer from "~/server/store.server";
-import { logout, requireUserId } from "~/server/auth.server";
 import DefaultLayout from "~/components/layout/DefaultLayout";
-import { handleLogout } from "~/utils/auth.util";
 
 export const links = () => [{ rel: "stylesheet", href: indexStyles }];
 
-export async function loader({ request }) {
-    return null;
-}
-
-export async function action({ request }) {
-    if(request.method === "POST") {
-        const data = {
-            ...Object.fromEntries(await request.formData()),
-        };
-        if(data._action === 'logout') {
-            return logout(request);
-        }
-    }
-    return null;
-}
-
 export default function Admin() {
-    const submit = useSubmit();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('accessToken');
+        navigate('/');
+    }
 
     return (
         <CustomPolarisAppProvider>
-            <DefaultLayout handleLogout={() => handleLogout(submit)}>
+            <DefaultLayout handleLogout={handleLogout}>
                 <Outlet />
             </DefaultLayout>
         </CustomPolarisAppProvider>
